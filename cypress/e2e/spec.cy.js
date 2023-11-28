fab206071f38ab63bd477b87035489fe9ec88e8f
Lisa Acosta <31046847+Lisa-Acosta@users.noreply.github.com>
2023-11-28 20:06:51 -0300

    Correccion de reportes y flakiness

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
+import { LoginMethods } from "./pages/login/login.methods";
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const user = 'random01';
+    const pass = 'random01';
+    cy.visit('https://www.demoblaze.com/index.html');
+    cy.get('a[data-target="#logInModal"]').click();
+    LoginMethods.login(user, pass);
+    cy.get('a#nameofuser').should('contain.text', user);
+    cy.wait(10000);
   })
 })
